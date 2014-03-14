72880b19be25414a3893a27ca312de193d3a6003
murali <banadabadmanaban@moneygram.com>
2014-03-14 12:53:22 +0530

    initial commit

---
+++ TestHudson/src/com/test/hudson/Test1.java
@@ -0,0 +1,14 @@
+package com.test.hudson;
+
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hi");
+	}
+
+}
